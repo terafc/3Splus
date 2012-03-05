0692b0c91712bb4e2d7ee1a3d51e074c5450a44f
ROCHEFEUILLE <g.rochefeuille@rt-iut.re>
2012-03-05 15:51:37 +0400

    Mes modifs

---
+++ application/view/footer.php
@@ -0,0 +1,16 @@
+
+<head>
+
+  <link rel="stylesheet" href="menu_design.css" />
+
+</head> 
+	
+		
+   		<div id="footer">
+			 <div id="s"> 3SPlus vous est propos&eacute; par les &eacute;tudiants de la promotion RT2 2011/2012 du TP n&ordm;3 <br>
+			&copy; 2012 |  <a href="http://www.iut-lareunion.fr/">IUT de la R&eacute;union</a> <a href="http://www.univ-reunion.fr/">Universit&eacute; de la R&eacute;union</a> 
+			</div>
+		</div>  
+	
+	
+</html>  
