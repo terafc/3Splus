cd9443a0c29d403a6bb516e7778c0a6f450356c1
Marcoz Francel <f.marcoz@rt-iut.re>
2012-03-19 15:57:10 +0300

    Modification des models

--- administration/model/productModel.php
+++ administration/model/productModel.php
@@ -1,33 +1,25 @@
 <?php
-	//Permet d'obtenir la liste des produits en fonction de la catégorie
-	function get_product_by_cat($id_category){
-		$bdd = Database3Splus::getinstance();//connexion();
-		$req = "SELECT * FROM products WHERE id_category = :id_category";
-		$result = $bdd->prepare($req);
-		$result->bindParam(':id_category', $id_category);
-		$result->execute();
-		$product = $result->fetchAll();
-		if(!empty($product)){//Si il n'y a aucun produit dans la catégorie
-			return $product;
-		}
-		else{
-			return FALSE;
-		}
+//Contient toutes les fonctions concernant la table product
+
+//Permet d'obtenir la liste des produits en fonction de la catégorie
+function get_product_by_cat($id_category) {
+	$bdd = Database3Splus::getinstance();
+	//connexion();
+	$req = "SELECT * FROM products WHERE id_category = :id_category";
+	$result = $bdd -> prepare($req);
+	$result -> bindParam(':id_category', $id_category);
+	$result -> execute();
+	$product = $result -> fetchAll();
+	if (!empty($product)) {//Si il n'y a aucun produit dans la catégorie
+		return $product;
 	}
+	else {
+		return FALSE;
+	}
+}
+// Permet d'ajouter un produit en prenant comme arguments :
+// 	-le nom du produit, la description,le prix
+function add_product($name,$description,$price){
 	
-	//Permet de vérifier que l'id du produit existe lors de l'ajout au panier
-	function verif_product($id_product){
-		$bdd = Database3Splus::getinstance();//connexion();
-		$req = "SELECT id_product FROM products WHERE id_product = :id_product";
-		$result = $bdd->prepare($req);
-		$result->bindParam(':id_product', $id_product);
-		$result->execute();
-		$colcount = $result->fetchColumn();//Récupère la première colonne depuis la ligne suivante d'un jeu de résultats
-		if($colcount != 0){
-			return TRUE;
-		}
-		else{
-			return FALSE;
-		}
-	}
+}
 ?>
