24a68bdcfef1f69e3a694d929091475399ad649c
ROCHEFEUILLE <g.rochefeuille@rt-iut.re>
2012-03-19 13:31:30 +0400

    Modification du head

--- application/view/head.php
+++ application/view/head.php
@@ -1,29 +1,30 @@
-
-	<head>
-
-		<link rel="stylesheet" href="menu_design.css" />
-		
-	</head>
-        <div id =" header">
-		<div class="head">
-			<img src="images/Logo.png" align="left">
+<head>
+	<link rel="stylesheet" href="css/menu_design.css" />
+</head>
+<div id =" header">
+	<div class="head">
+		<img src="img/Logo.png" align="left">
+	</div>
+	<div id="boutique">
+		<div id="b">
+			<p>
+				B
+			</p>
 		</div>
-
-                     <div id="boutique">
-                 <div id="b"><p>B</p></div><p>outique </p>
-                </div>
-            <div id="bloc2">
-                </div>
-		<div>
-			<table id="menuHeader">
-                          
-                            <tr>
-					<td><span><a href="SandwichView.php">Sandwic</a></span></td>
-					<td><span><a href="ViennoiserieView.php">Viennoiserie</a></span></td>
-					<td><span><a href="SaladeView.php">Salade</a></span></td>
-					<td><span><a href="GlaceView.php">Glace</a></span></td>
-					<td><span><a href="BoissonView.php">Boisson</a></span></td>
-                            </tr>
-			</table>
-		</div>
-        </div>
+		<p>
+			outique
+		</p>
+	</div>
+	<div id="bloc2"></div>
+	<div>
+		<table id="menuHeader">
+			<tr>
+				<td><span><a href="SandwichView.php">Sandwichs</a></span></td>
+				<td><span><a href="ViennoiserieView.php">Viennoiseries</a></span></td>
+				<td><span><a href="SaladeView.php">Salades</a></span></td>
+				<td><span><a href="GlaceView.php">Glaces</a></span></td>
+				<td><span><a href="BoissonView.php">Boissons</a></span></td>
+			</tr>
+		</table>
+	</div>
+</div>
