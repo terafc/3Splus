9951e6b3a75f3a236703a09e1f8468fdce81bef2
clement <c.nandjan@rt-iut.re>
2012-03-20 14:53:50 +0400

    La page d accueil est le login

--- administration/view/login.php
+++ administration/view/login.php
@@ -1,9 +1,12 @@
 <p><h1>Bienvenue sur l'application 3S+ !</h1></p>
 <p>
 	<form method='POST' action=''>
-		Veuillez entrer votre login: <input type='text' name=''/>
+		Veuillez entrer votre login: 
+		<br />
+		<input type='text' name=''/>
 		<br/>
-		Et votre mot de passe: <input type='password' name=''/>		
+		Et votre mot de passe: 
+		<br /><input type='password' name=''/>		
 	</form>
 	
 </p>
