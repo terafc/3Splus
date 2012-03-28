d2db706365870cceee80abc69c34a172bb918d62
Marc Dijoux <m.dijoux@rt-iut.re>
2012-03-28 21:40:35 +0400

    modification de la vue commandes.php

--- administration/view/commandes.php
+++ administration/view/commandes.php
@@ -1,32 +1,11 @@
-<?php
-	
-	$CMD[0]=array("Bouchon Gratine","K", "Fanta 50cl","DIJOUX", "Marc","3100263");
-	$CMD[1]=array("Americain Poulet","M", "Coca 50cl","NANDJAN", "Clement","3000302");
-	$CMD[2]=array("Americain Jambon","P", "Edena 1l","RETHORE","Sof", "3100325");
-	$CMD[3]=array("Americain nature","KP", "Sprite 50cl", "MARCOZ", "Francel", "3000254");
-	
-	$Numcmd="026256";
-	$NomGroupe="Roti Porc";
-	$Porteur="DIJOUX Marc";
-	$NumetdPorteur="3100263";
-	
-	$cmds[0]['commandes'] = $CMD;
-	$cmds[0]['num_cmd'] = $Numcmd;
-	$cmds[0]['nom_grp'] = $NomGroupe;
-	$cmds[0]['nom_porteur'] = $Porteur;
-	$cmds[0]['num_porteur'] = $NumetdPorteur;
-	
-	$cmds[1]=$cmds[0];
-	function get_url_validate_by_gid($Gid){
-		return HTTP_INDEX.'?page=product&action=validate&id='.$Gid;
-	}
-?>
 <div class="menuleft">
 	<div class="textmenuleft">
 		<b class="titremenuleft"><u>
 			Groupes</u></b>
 		<br />
-		<u><?php echo $NomGroupe; ?></u>
+		<?php foreach ($cmds as $groupe){ ?>
+		<u><?php echo $groupe['nom_grp']; ?></u>
+		<?php } ?>
 	</div>
 </div>
 <div class="tableaux">	
@@ -35,6 +14,10 @@
 		<b>Groupe: <span class="colorPink"><?php echo $groupe['nom_grp']; ?></span></b>
 		<b>Porteur: <span class="colorBlue"><?php echo $groupe['nom_porteur']; ?></span></b>
 		<b>N° d'etudiant: <?php echo $groupe['num_porteur']; ?></b>
+		
+		<form methode=POST, action='<?php echo get_url_validate_by_gid($groupe['num_cmd']); ?>'>
+			<input type='submit' name='cmdvalider' value='Valider la commande' class="floatRight">
+		</form>
 		<table class='groupe'>
 			<tr>
 				<th class='cadrehead'>Sandwichs</th>
@@ -82,9 +65,6 @@
 			<?php $i++; 
 			}
 			?>
-			<form methode=POST, action='<?php echo get_url_validate_by_gid($groupe['num_cmd']) ?>'>
-				<input type='submit' name='cmdvalider' value='Valider la commande' class="floatRight">
-			</form>
 
 		</table>
 	</div>
