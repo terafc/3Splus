341cda931658ae34b3f1d3efd9e049d0a8507472
Marc Dijoux <m.dijoux@rt-iut.re>
2012-04-09 14:39:00 +0400

    blabla

--- administration/controller/indivorderController.php
+++ administration/controller/indivorderController.php
@@ -3,14 +3,7 @@
 	require_once (CHEMIN_MODEL . '/mainModel.php');
 	switch($_REQUEST['action']){
 		case 'show':
-
-			$CMD = array();
-
-			$CMD[0]=array("Bouchon Gratine","K", "Fanta 50cl","DIJOUX", "Marc","3100263","123456");
-			$CMD[1]=array("Americain Poulet","M", "Coca 50cl","NANDJAN", "Clement","3000302","123457");
-			$CMD[2]=array("Americain Jambon","P", "Edena 1l","RETHORE","Sof", "3100325","123458");
-			$CMD[3]=array("Americain nature","KP", "Sprite 50cl", "MARCOZ", "Francel", "3000254","123459");
-			$CMD[4]=array("Crudite Jambon","KM", "Sambo 33cl", "RIVIERE", "Serge", "3000255","123460");
+			$indiv_orders = get_users_cmd();
 			$validate_url = HTTP_INDEX.'?page=indivorder&action=validate';
 
 			include_once(CHEMIN_VIEW . '/header.php');
