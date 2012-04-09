915d59bdc43148efcfd3d82fa2d004c705734968
Marcoz Francel <f.marcoz@rt-iut.re>
2012-04-09 18:41:08 +0300

    Ajout model get_users() et get_cmd

--- administration/model/mainModel.php
+++ administration/model/mainModel.php
@@ -1,6 +1,6 @@
 <?php
- //include_once ('../global/config.inc.php');
- //include_once ('../global/Database.php');
+include_once ('../global/config.inc.php');
+include_once ('../global/Database.php');
 
 // Renvoie un tableau contenant le nom des groupes qui ont commandés,les produits associé et le porteur
 function get_groups() {
@@ -17,16 +17,16 @@ function get_groups() {
 		$groups[$value['name']]['users'][$value['id_user']]['prenoms'] = $value['prenoms'];
 		$groups[$value['name']]['product'] = get_cmd_groups($groups[$value['name']]['id_group']);
 	}
-	//Pour determiner le porteur (en se basant sur les id_order contenue dans le précédent tableau groups 
+	//Pour determiner le porteur (en se basant sur les id_order contenue dans le précédent tableau groups
 	foreach ($groups as $key => $value) {
-		$req_carrier="select id_user from carrier where id_order in
+		$req_carrier = "select id_user from carrier where id_order in
 						(select id_order from orders where id_user in
-						(select id_user from users,groups where users.id_group=groups.id_group and groups.id_group=".$value['id_group'].") and paid=1)";
-						var_dump($key);
+						(select id_user from users,groups where users.id_group=groups.id_group and groups.id_group=" . $value['id_group'] . ") and paid=1)";
+		var_dump($key);
 		foreach ($bdd->query($req_carrier) as $carrier => $id) {
-			$groups[$key]['carrier']['id_user']=$id['id_user'];
-			$groups[$key]['carrier']['nom']=$groups[$key]['users'][$id['id_user']]['nom'];
-			$groups[$key]['carrier']['prenoms']=$groups[$key]['users'][$id['id_user']]['prenoms'];
+			$groups[$key]['carrier']['id_user'] = $id['id_user'];
+			$groups[$key]['carrier']['nom'] = $groups[$key]['users'][$id['id_user']]['nom'];
+			$groups[$key]['carrier']['prenoms'] = $groups[$key]['users'][$id['id_user']]['prenoms'];
 		}
 	}
 	return $groups;
@@ -54,43 +54,51 @@ function get_cmd_groups($id_group) {
 	return $result;
 }
 
-//Renvoie un tableau contenant les utlisateurs(toutes les informations) qui ont commandés et les produits associés avec la quantité et la sauce 
-function get_users_cmd() {
+//Renvoie un tableau contenant les utlisateurs(toutes les informations) qui ont commandés et les id_order
+function get_users() {
 	$result = array();
-	$users=array();
-	$cmd=array();
+	$users = array();
+	$cmd = array();
 	$bdd = Database3Splus::getInstance();
 	$req = "select id_order,id_user from orders where paid=1";
 	$i = 0;
 	foreach ($bdd->query($req) as $infos => $value) {
-		//$result[$value['id_user']][]=$value['id_order'];
-		$req_user='select * from users where id_user='.$value['id_user'];
+		$result[$value['id_user']]['id_order'][] = $value['id_order'];
+		$req_user = 'select * from users where id_user=' . $value['id_user'];
 		foreach ($bdd->query($req_user) as $user => $detail) {
-			$result[$value['id_user']]['nom']=$detail['firstname'];
-			$result[$value['id_user']]['prenoms']=$detail['lastname'];
-			$result[$value['id_user']]['email']=$detail['email'];
-			$result[$value['id_user']]['id_group']=$detail['id_group'];
-			$result[$value['id_user']]['id_facebook']=$detail['id_facebook'];
-			$result[$value['id_user']]['point']=$detail['point'];
-		}
-		$req_cmd="SELECT DISTINCT products.name, category.name as categorie, order_details.amount as qtt,order_details.sauce
-				FROM order_details,products,category
-				WHERE products.id_product=order_details.id_product and products.id_category=category.id_category and order_details.id_order=".$value['id_order'];
-				$i=0;
-		foreach ($bdd->query($req_cmd) as $product => $info) {
-			$result[$value['id_user']]['orders'][$value['id_order']][$i]['nom']=$info['name'];
-			$result[$value['id_user']]['orders'][$value['id_order']][$i]['categorie']=$info['categorie'];
-			$result[$value['id_user']]['orders'][$value['id_order']][$i]['qtt']=$info['qtt'];
-			$result[$value['id_user']]['orders'][$value['id_order']][$i]['sauce']=$info['sauce'];
-			$i++;
+			$result[$value['id_user']]['nom'] = $detail['firstname'];
+			$result[$value['id_user']]['prenoms'] = $detail['lastname'];
+			$result[$value['id_user']]['email'] = $detail['email'];
+			$result[$value['id_user']]['id_group'] = $detail['id_group'];
+			$result[$value['id_user']]['id_facebook'] = $detail['id_facebook'];
+			$result[$value['id_user']]['point'] = $detail['point'];
 		}
 	}
 	return $result;
 }
+
+//Renvoie les details des commandes selon l'id_order => le nom:name, le nom de la categorie:category, la quantité:qtt et la sauce si existant
+function get_cmd($id_order) {
+	$result = array();
+	$bdd = Database3Splus::getInstance();
+	$req = "SELECT DISTINCT products.name, category.name, order_details.amount as qtt,order_details.sauce
+				FROM order_details,products,category
+				WHERE products.id_product=order_details.id_product and products.id_category=category.id_category and order_details.id_order=" . $id_order;
+	$i=0;
+	foreach ($bdd->query($req) as $cmd => $info) {
+		$result[$i]['name'] = $info['name'];
+		$result[$i]['category'] = $info['name'];
+		$result[$i]['qtt'] = $info['qtt'];
+		$result[$i]['sauce'] = $info['sauce'];
+		$i++;
+	}
+	return $result;
+}
+
 // echo "<pre>";
 // print_r(get_groups());
 // echo "</pre>";
-/*echo "<pre>";
-print_r(get_groups());
-echo "</pre>";*/
+echo "<pre>";
+print_r(get_cmd(27));
+echo "</pre>";*
 ?>
